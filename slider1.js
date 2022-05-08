c5a98489b9cc119c3a4e9ebe92acaba8c77edb4b
Marco Berna <mbernaildeons@gmail.com>
2022-05-08 01:25:33 -0500

    codigo recuperado de los sliders

--- slider1.js
+++ slider1.js
@@ -1,3 +1,4 @@
+const contenedorSlider = document.querySelector("#contenedor-slider")
 const slider =document.querySelector("#slider");
 let sections= document.querySelectorAll(".contenedor-zapatilla");
 let sectionLast = sections[sections.length - 1];
@@ -7,27 +8,47 @@ slider.insertAdjacentElement("afterbegin", sectionLast);
 
 function next(){
     let sectionFirst= document.querySelectorAll(".contenedor-zapatilla")[0];
-    slider.style.marginLeft = "-200%";
-    slider.style.transition = "margin-left .5s ease";
-    setTimeout(function(){
-        slider.style.marginLeft= "-100%";
-        slider.style.transition="none";
-        slider.insertAdjacentElement("beforeend",sectionFirst);
-    },500)
     
+    if (contenedorSlider.clientWidth < 1000) {
+        slider.style.marginLeft = "-200%";
+        slider.style.transition = "margin-left .5s ease";
+        setTimeout(function(){
+            slider.style.marginLeft= "-100%";
+            slider.style.transition="none";
+            slider.insertAdjacentElement("beforeend",sectionFirst);
+        },500)
+        
+    }
+    else{
+        slider.style.marginLeft = "-100%";
+        slider.style.transition = "margin-left .5s ease";
+        setTimeout(function(){
+            slider.style.marginLeft= "-50%";
+            slider.style.transition="none";
+            slider.insertAdjacentElement("beforeend",sectionFirst);
+        },500)
+    }
 }
 
 function back(){
     let sectionLast2 = document.querySelectorAll(".contenedor-zapatilla")[document.querySelectorAll(".contenedor-zapatilla").length-1];
-    slider.style.marginLeft = "0%";
-    slider.style.transition="margin-left .5s ease";
-    setTimeout(function(){
-        slider.style.transition= "none"
-        slider.style.marginLeft = "-100%"
-        slider.insertAdjacentElement("afterbegin", sectionLast2);
+    if (contenedorSlider.clientWidth < 1000) {
+        slider.style.marginLeft = "0%";
+        slider.style.transition="margin-left .5s ";
+        setTimeout(function(){
+            slider.style.transition= "none"
+            slider.style.marginLeft = "-100%"
+            slider.insertAdjacentElement("afterbegin", sectionLast2);
     }, 500)
+    }
+    else{
+        slider.style.marginLeft = "0%";
+        slider.style.transition = "margin-left .5s"
+        setTimeout(function(){
+            slider.style.marginLeft ="-50%";
+            slider.style.transition = "none";
+            slider.insertAdjacentElement("afterbegin", sectionLast2)
+        }, 500);
+    }
 
 }
-setInterval(function(){
-    next();
-}, 7000)
--- slider2.js
+++ slider2.js
@@ -1,3 +1,4 @@
+const contenedorSliderRun = document.querySelector("#contenedor-slider-run")
 const sliderRun = document.querySelector("#slider-run");
 const sectionsRun = document.querySelectorAll("#section-run");
 const sectionLastRun = sectionsRun[sectionsRun.length-1];
@@ -8,21 +9,43 @@ sliderRun.insertAdjacentElement("afterbegin",sectionLastRun);
 // funcion para que el primer contenedor se vaya al ultimo pulsando el boton right
 function nextRun() {
     let firstRun = document.querySelectorAll("#section-run")[0]; 
-    sliderRun.style.marginLeft ="-200%";
-    sliderRun.style.transition = "margin-left 1s"
-    setTimeout(function() {
-    sliderRun.style.marginLeft = "-100%";
-    sliderRun.style.transition = "none"
-    sliderRun.insertAdjacentElement("beforeend",firstRun)
-},1000)
+    if (contenedorSliderRun.clientWidth < 1000) {
+        sliderRun.style.marginLeft ="-200%";
+        sliderRun.style.transition = "margin-left 1s"
+        setTimeout(function() {
+        sliderRun.style.marginLeft = "-100%";
+        sliderRun.style.transition = "none"
+        sliderRun.insertAdjacentElement("beforeend",firstRun)
+        },1000)
+    }
+    else{
+        sliderRun.style.marginLeft ="-100%";
+        sliderRun.style.transition = "margin-left 1s"
+        setTimeout(function() {
+        sliderRun.style.marginLeft = "-50%";
+        sliderRun.style.transition = "none"
+        sliderRun.insertAdjacentElement("beforeend",firstRun)
+        },1000)
+    }
 }
 function backRun(){
     let lastRun = document.querySelectorAll("#section-run")[document.querySelectorAll("#section-run").length-1];
-    sliderRun.style.marginLeft ="0%"
-    sliderRun.style.transition = "margin-left 1s"
-    setTimeout(function(){
+    if (contenedorSliderRun.clientWidth < 1000) {
+        sliderRun.style.marginLeft ="0%"
+        sliderRun.style.transition = "margin-left 1s"
+        setTimeout(function(){
         sliderRun.style.marginLeft = "-100%";
         sliderRun.style.transition = "none";
         sliderRun.insertAdjacentElement("afterbegin",lastRun);
-    },1000)
+        },1000)
+    }
+    else{
+        sliderRun.style.marginLeft ="0%"
+        sliderRun.style.transition = "margin-left 1s"
+        setTimeout(function(){
+        sliderRun.style.marginLeft = "-50%";
+        sliderRun.style.transition = "none";
+        sliderRun.insertAdjacentElement("afterbegin",lastRun);
+        },1000)
+    }
 }
